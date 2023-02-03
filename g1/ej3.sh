1620143437e6de739c448f9859dd5d5ad2a2151b
kikso <quiquevolc@gmail.com>
2023-02-03 12:57:44 +0100

    Ejecicio 3 hecho

--- g1/ej3.sh
+++ g1/ej3.sh
@@ -1,1 +1,22 @@
 #! /bin/bash
+
+win=0;
+pro1=0;
+linx=0;
+pro2=0;
+
+while read user os value; do
+
+    if [ "$os" == "Windows" ];then
+        
+        ((win++));
+        ((pro1 += value));
+    else
+        ((linx++));
+        ((pro2 += value));
+    fi
+
+done < ./../ficheros_scripts/listado.txt
+
+echo "Windows -> $win $pro1";
+echo "Linux -> $linx $pro2";
